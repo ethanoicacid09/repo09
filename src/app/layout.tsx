174e356ec4c60e9465a8e37e3419f696e149cf5a
ethanoicacid09 <sakshibhadke6@gmail.com>
2026-04-20 12:57:44 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: {
-    default: "ZenHerb — Herbs & Yoga Essentials",
+    default: "ZenHerb — Natural wellness Meets Mindful Living",
     template: "%s | ZenHerb",
   },
   description:
